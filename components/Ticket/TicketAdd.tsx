274064cc2d6f2cd5e81e70740e97017744c3615a
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-10-23 22:39:41 +0900

    [Ticket] 티켓 분류 추가, 카테고리 별 선택 기능 추가

--- components/Ticket/TicketAdd.tsx
+++ components/Ticket/TicketAdd.tsx
@@ -1,54 +1,116 @@
-import React, { useState } from 'react';
-import { View, Button, Text, FlatList } from 'react-native';
-import { TextInput } from "react-native-paper";
+import React, { useState } from "react";
+import { View, Button, Text, FlatList } from "react-native";
+import { Menu, Provider, TextInput } from "react-native-paper";
 
 const TicketAdd: React.FC = () => {
-  const [ticketName, setTicketName] = useState('');
-  const [price, setPrice] = useState('');
-  const [seatInfo, setSeatInfo] = useState('');
-  const [tickets, setTickets] = useState<Array<{ name: string, price: string, seat: string }>>([]);
+  const [ticketName, setTicketName] = useState("");
+  const [price, setPrice] = useState("");
+  const [seatInfo, setSeatInfo] = useState("");
+  const [tickets, setTickets] = useState<
+  Array<{ name: string; price: string; seat: string; category: string }>
+>([]);
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("카테고리 선택");
 
   const handleSubmit = () => {
     if (ticketName && price && seatInfo) {
-      setTickets([...tickets, { name: ticketName, price, seat: seatInfo }]);
-      setTicketName('');
-      setPrice('');
-      setSeatInfo('');
+      setTickets([...tickets, { name: ticketName, price, seat: seatInfo, category: selectedCategory }]);
+      setTicketName("");
+      setPrice("");
+      setSeatInfo("");
     }
   };
 
   return (
-    <View style={{ padding: 16, flex: 1 }}>
-      <TextInput
-        label="티켓 이름"
-        value={ticketName}
-        onChangeText={setTicketName}
-      />
-      <TextInput
-        label="가격"
-        value={price}
-        onChangeText={setPrice}
-        keyboardType="numeric"
-      />
-      <TextInput
-        label="좌석 정보"
-        value={seatInfo}
-        onChangeText={setSeatInfo}
-      />
-      <Button title="등록" onPress={handleSubmit} />
+    <Provider>
+      <View style={{ padding: 16, flex: 1 }}>
+        <Menu
+          visible={menuVisible}
+          onDismiss={() => setMenuVisible(false)}
+          anchor={
+            <Button
+              title={selectedCategory}
+              onPress={() => setMenuVisible(true)}
+            />
+          }
+        >
+          <Menu.Item
+            onPress={() => {
+              setSelectedCategory("원가 미만");
+              setMenuVisible(false);
+            }}
+            title="원가 미만"
+          />
+          <Menu.Item
+            onPress={() => {
+              setSelectedCategory("콘서트");
+              setMenuVisible(false);
+            }}
+            title="콘서트"
+          />
+          <Menu.Item
+            onPress={() => {
+              setSelectedCategory("스포츠");
+              setMenuVisible(false);
+            }}
+            title="스포츠"
+          />
+          <Menu.Item
+            onPress={() => {
+              setSelectedCategory("뮤지컬/연극");
+              setMenuVisible(false);
+            }}
+            title="뮤지컬/연극"
+          />
+          <Menu.Item
+            onPress={() => {
+              setSelectedCategory("영화/전시");
+              setMenuVisible(false);
+            }}
+            title="영화/전시"
+          />
+        </Menu>
+        <TextInput
+          label="티켓 이름"
+          value={ticketName}
+          onChangeText={setTicketName}
+        />
+        <TextInput
+          label="가격"
+          value={price}
+          onChangeText={setPrice}
+          keyboardType="numeric"
+        />
+        <TextInput
+          label="좌석 정보"
+          value={seatInfo}
+          onChangeText={setSeatInfo}
+        />
+        <Button title="등록" onPress={handleSubmit} />
 
-      <FlatList
-        data={tickets}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={{ marginTop: 16, padding: 8, borderColor: 'gray', borderWidth: 1, borderRadius: 8 }}>
-            <Text>티켓 이름: {item.name}</Text>
-            <Text>가격: {item.price}</Text>
-            <Text>좌석 정보: {item.seat}</Text>
-          </View>
-        )}
-      />
-    </View>
+        <FlatList
+          data={tickets}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                marginTop: 16,
+                padding: 8,
+                borderColor: "gray",
+                borderWidth: 1,
+                borderRadius: 8,
+              }}
+            >
+              <Text>분류: {item.category}</Text>
+              <Text>티켓 이름: {item.name}</Text>
+              <Text>가격: {item.price}</Text>
+              <Text>좌석 정보: {item.seat}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </Provider>
   );
 };
 
